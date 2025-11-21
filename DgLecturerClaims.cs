c03202d9bf689c7c96c7b82bd109e42dffbdaf5b
liamfranz <162138432+liamfranz@users.noreply.github.com>
2025-11-21 16:59:20 +0200

    Add files via upload

---
+++ DgLecturerClaims.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ClaimSystem
+{
+    internal class DgLecturerClaims
+    {
+        internal static ObservableCollection<Claim> ItemsSource;
+    }
+}
